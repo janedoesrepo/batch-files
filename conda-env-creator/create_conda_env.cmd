1bf58a188c205d3d922a292a80f930ccf0700123
Jan Niklas Engel <engeljanniklas@gmail.com>
2020-12-06 12:24:22 +0100

    Update create_conda_env.cmd

--- conda-env-creator/create_conda_env.cmd
+++ conda-env-creator/create_conda_env.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-rem check if two but not three parameters are provided
+:: check if two but not three parameters are provided
 If not "%~2"=="" if "%~3"=="" goto runscript
 echo ERROR: This script requires the following parameters:
 echo  - env-name   (the name of the environment, e.g. tensorflow)
@@ -12,19 +12,19 @@ goto :EOF
 
 :runscript
 
-rem set variables
+:: set variables
 set env-name=%1
 set py-version=%2
 
-rem create and activate environment
-cmd /c conda create -y --name %env-name% python=%py-version%
+:: create and activate environment
+call conda create -y --name %env-name% python=%py-version%
 call conda activate %env-name%
 
-rem install nb extensions and register ipykernel
+:: install nb extensions and register ipykernel
 cmd /c conda install -y nb_conda
 cmd /c python -m ipykernel install --user --name %env-name% --display-name "Python %py-version% (%env-name%)"
 
-rem deactivate environment and check the list of environments
-cmd /c conda deactivate
+:: deactivate environment and check the list of environments
 cmd /c conda env list
+
 echo Environment %env-name% (Python %py-version%) successfully created
