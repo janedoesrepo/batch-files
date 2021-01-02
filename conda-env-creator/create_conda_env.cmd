d199c57d8dc84c705b712685c4aadd95d3ea95e6
Jan Niklas Engel <captndata@gmail.com>
2021-01-02 17:14:54 +0100

    Change call to cmd /c statement

--- conda-env-creator/create_conda_env.cmd
+++ conda-env-creator/create_conda_env.cmd
@@ -2,9 +2,10 @@
 
 :: check if two but not three parameters are provided
 If not "%~2"=="" if "%~3"=="" goto runscript
-echo ERROR: This script requires the following parameters:
-echo  - env-name   (the name of the environment, e.g. tensorflow)
-echo  - py-version (the desired python version, e.g. 3.7)
+echo ERROR: Could not create environment!
+echo Please provide the following parameters:
+echo  - env-name:   name of the environment (e.g. tensorflow)
+echo  - py-version: the python version to use (e.g. 3.7)
 echo Example usage:
 echo  %~nx0 tensorflow 3.7
 goto :EOF
@@ -14,16 +15,12 @@ goto :EOF
 :: set variables
 set env-name=%1
 set py-version=%2
+set display-name="%env-name% (Python %py-version%)"
 
 :: create and activate environment
-call conda create -n %env-name% -y python=%py-version%
+cmd /c conda create -n %env-name% -y python=%py-version%
 call conda activate %env-name%
 
 :: install nb extensions and register ipykernel
 cmd /c conda install -y nb_conda
-cmd /c python -m ipykernel install --user --name %env-name% --display-name "Python%py-version% (%env-name%)"
-
-:: check the list of environments
-cmd /c conda env list
-
-echo Environment %env-name% (Python %py-version%) successfully created
+cmd /c python -m ipykernel install --user --name %env-name% --display-name %display-name%
